a7ff726990156e31784ab1764d8a3477c6915800
herisusantoarismann <heri.s.arisman@gmail.com>
2022-07-12 16:17:20 +0700

    Add title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from "next";
+import Head from "next/head";
 import Content from "../components/Content/Content";
 import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/header";
@@ -6,13 +7,18 @@ import MainLayout from "../layouts/main";
 
 const Home: NextPage = () => {
   return (
-    <MainLayout>
-      <div>
-        <Header />
-        <Content />
-      </div>
-      <Footer />
-    </MainLayout>
+    <>
+      <Head>
+        <title>Translation App</title>
+      </Head>
+      <MainLayout>
+        <div>
+          <Header />
+          <Content />
+        </div>
+        <Footer />
+      </MainLayout>
+    </>
   );
 };
 
